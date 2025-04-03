1a8ac1e2eb7b078945b3179c8628ce48bbd5b01e
Joao Pedro <jpbentosperandio@gmail.com>
2025-04-03 00:30:39 -0300

    commit user profile view

--- resources/views/acount.blade.php
+++ resources/views/acount.blade.php
@@ -2,18 +2,63 @@
 <body>
     <div class="w-100 h-100 body-container d-flex flex-column justify-content-center">
         @include('template.nav')
+        @if(Auth::user()->nivel === 0)
+            <main class="acount-main">
+                <button class="btn-voltar">
+                    <i class="bi bi-arrow-left"></i>
+                </button>
+                <div class="profile-container">
+                    <p class="profile-container-title">Meu Perfil</p>
+                    <div class="profile-infos">
+                        <section>
+                            <div class="profile-user-image">
+                                <img src="{{asset('images/users/usuario0.png')}}" alt="usuario 0">
+                            </div>
+                            <p class="user-email">{{Auth::user()->email}}</p>
+                        </section>
+                        <section>
+                            <h1 class="user-name">{{Auth::user()->nome}}</h1>
+                            <div class="theme-container">
+                                <p>Tema:</p>
+                                <div class="theme-select">
+                                    <div class="theme-option theme-active">Claro</div>
+                                    <div class="theme-option">Escuro</div>
+                                    <div class="theme-option">Dispositivo</div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </main>
+        @endif
         @if(Auth::user()->nivel === 1)
             <main>
-                
+                <button class="btn-voltar">
+                    <i class="bi bi-arrow-left"></i>
+                </button>
+                <div class="profile-container">
+                    
+                </div>
             </main>
         @endif
         @if(Auth::user()->nivel === 2)
             <main>
-                
+                <button class="btn-voltar">
+                    <i class="bi bi-arrow-left"></i>
+                </button>
+                <div class="profile-container">
+                    
+                </div>
             </main>
         @endif
         @if(Auth::user()->nivel === 4)
             <main>
+                <button class="btn-voltar">
+                    <i class="bi bi-arrow-left"></i>
+                </button>
+                <div class="profile-container">
+
+                </div>
                 
             </main>
         @endif
